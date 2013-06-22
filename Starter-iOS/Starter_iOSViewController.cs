b2757588c841e3d1ae606b463d042b7674bd363e
Paul Betts <paul@paulbetts.org>
2013-06-22 11:22:08 -0700

    Create a basically blank iOS project

---
+++ Starter-iOS/Starter_iOSViewController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace StarteriOS
+{
+    public partial class Starter_iOSViewController : UIViewController
+    {
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+
+        public Starter_iOSViewController()
+            : base (UserInterfaceIdiomIsPhone ? "Starter_iOSViewController_iPhone" : "Starter_iOSViewController_iPad", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+            
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
+        {
+            // Return true for supported orientations
+            if (UserInterfaceIdiomIsPhone)
+            {
+                return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
+
