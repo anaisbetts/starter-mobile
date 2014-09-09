1ce79e6fe165df6f32ee5002235529400753f62c
Paul Betts <paul@paulbetts.org>
2014-09-08 22:16:22 -0700

    Set up auto-suspend for Android

---
+++ Starter-Android/AndroidApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using ReactiveUI;
+
+namespace Starter
+{
+    [Application]
+    public class AndroidApplication : Application
+    {
+        AutoSuspendHelper autoSuspendHelper;
+        public AndroidApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle,transfer)
+        {
+            autoSuspendHelper = new AutoSuspendHelper(this);
+        }
+    }
+}
+
