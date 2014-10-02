36e1d866303ffdfeb1a063727e3bd52504659858
Paul Betts <paul@paulbetts.org>
2014-10-02 15:46:56 -0700

    iOS app is now 100% boilerplate

--- Starter-iOS/AppDelegate.cs
+++ Starter-iOS/AppDelegate.cs
@@ -3,43 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
-using Starter.Views;
 using ReactiveUI;
+using Starter.Core.ViewModels;
+using Xamarin.Forms;
 
 namespace Starter
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
     [Register ("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
-        // class-level declarations
         UIWindow window;
-        TestViewController viewController;
-		AutoSuspendHelper suspendHelper;
+        AutoSuspendHelper suspendHelper;
+        UIViewController vc;
 
-		public AppDelegate()
-		{
-			suspendHelper = new AutoSuspendHelper(this);
-		}
-
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public AppDelegate()
         {
-            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            RxApp.SuspensionHost.CreateNewAppState = () => new AppBootstrapper();
+        }
 
-            viewController = new TestViewController();
-            window.RootViewController = viewController;
-            window.MakeKeyAndVisible();
-            
-			suspendHelper.FinishedLaunching(app, options);
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+        {
+            Forms.Init ();
+            RxApp.SuspensionHost.SetupDefaultSuspendResume();
+
+            suspendHelper = new AutoSuspendHelper(this);
+            suspendHelper.FinishedLaunching(app, options);
+
+            window = new UIWindow (UIScreen.MainScreen.Bounds);
+            var bootstrapper = RxApp.SuspensionHost.GetAppState<AppBootstrapper>();
+
+            window.RootViewController = bootstrapper.CreateMainPage().CreateViewController();
+            window.MakeKeyAndVisible ();
+
             return true;
         }
 
@@ -54,4 +49,3 @@ namespace Starter
         }
     }
 }
-
